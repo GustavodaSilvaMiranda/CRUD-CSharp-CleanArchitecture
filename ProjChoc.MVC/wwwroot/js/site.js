824c8b9dfcd7627110a92cb3f8e870f9a4736cad
Gustavo Miranda <gugamiranda617@gmail.com>
2024-09-10 13:19:04 -0300

    Mudanças Básicas de informação

--- ProjChoc.MVC/wwwroot/js/site.js
+++ ProjChoc.MVC/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+}
